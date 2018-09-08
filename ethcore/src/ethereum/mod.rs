a670db5372b94c00cedcabdd8b7b95c8e71399ce
Afri Schoedon <5chdn@users.noreply.github.com>
2018-09-08 18:27:07 +0200

    parity: add --chain goerli (#2)
    * ethcore: add goerli chainspec
    
    * ethcore: allow using goerli chain spec
    
    * cli: add goerli to cli description
    
    * parity: add goerli chain to params
    
    * rpc(debug_getBadBlocks): fix test (#9502)

--- ethcore/src/ethereum/mod.rs
+++ ethcore/src/ethereum/mod.rs
@@ -112,6 +112,11 @@ pub fn new_sokol<'a, T: Into<SpecParams<'a>>>(params: T) -> Spec {
 	load(params.into(), include_bytes!("../../res/ethereum/poasokol.json"))
 }
 
+/// Create a new Goerli testnet chain spec.
+pub fn new_goerli<'a, T: Into<SpecParams<'a>>>(params: T) -> Spec {
+	load(params.into(), include_bytes!("../../res/ethereum/goerli.json"))
+}
+
 // For tests
 
 /// Create a new Foundation Frontier-era chain spec as though it never changes to Homestead.
